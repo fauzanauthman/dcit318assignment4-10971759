642825a7ff05b9a53af4f23a9400191afee505f3
Osman  Fauzan <103241771+fauzanauthman@users.noreply.github.com>
2024-07-09 17:53:17 +0000

    I created a panel for drawing. It allows the user to draw anything of thier liking using the mouse buttons to navigate and draw any shape.

---
+++ CanvasDrawApp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+
+namespace CanvasDrawApp
+{
+
+    public partial class Form1 : Form
+    {
+        private Point startPoint;
+        private Point endPoint;
+        private Pen pen;
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Black, 2);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPoint = e.Location;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                endPoint = e.Location;
+                panel1.Refresh();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            endPoint = e.Location;
+            DrawLine(startPoint, endPoint);
+        }
+
+        private void DrawLine(Point startPoint, Point endPoint)
+        {
+            using (Graphics g = panel1.CreateGraphics())
+            {
+                g.DrawLine(pen, startPoint, endPoint);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
